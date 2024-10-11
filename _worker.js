ce6e632aa934d96a6843f8623b7278c94ef929a4
suexcxine <suex.bestwishes@gmail.com>
2024-10-11 12:16:58 +0900

    fix

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ export default {
     const url = new URL(request.url);
     
     // 构造新的URL，将路径和查询参数转发到app.voxsig.com
-    const newUrl = new URL(url.pathname + url.search, 'https://app.voxsig.com');
+    const newUrl = new URL(url.pathname + url.search, 'https://app.qiabot.com');
     
     // 创建新的请求，保留原始请求的方法、头部等信息
     const newRequest = new Request(newUrl, {
